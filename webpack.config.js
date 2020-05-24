5f79c596af346931d3cbb813cba3ca2e5d466f72
Jim Kim <magnking@gmail.com>
2020-05-24 19:49:34 +0900

    test

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+/**
+ * Created by user on 2020/05/24.
+ */
+const path = require('path');
+
+module.exports = {
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+
+    },
+    mode: 'development',
+    watch: true
+}
