7c733342fb44ebf98690c9c6e59e3bea84a7a32c
Jim <Jim Kim>
2020-06-16 14:42:50 +0900

    webpack- dev-sever functions,

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,3 +3,7 @@
  */
 import '../styles/styles.css';
 
+if (module.hot) {
+    module.hot.accept();
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,17 @@ module.exports = {
         path: path.resolve(__dirname, 'app')
 
     },
+    devServer: {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
+    },
     mode: 'development',
-    watch: true,
+    //watch: true,
     module: {
         rules: [
             {
